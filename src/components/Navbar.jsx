7500d20141c2bb69fea787c878d8d06975752aba
Abivarsan <ketheesabi7400@gmail.com>
2024-11-04 17:11:24 +0530

    navbar name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,9 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
            <p className="text-white text-[18px]
             font-bold cursor-pointer flex">
-            Ketheeswaran &nbsp; 
+            Abivarsan &nbsp; 
             <span className='sm:block hidden'>| 
-              Abivarsan</span></p>
+              Ketheeswaran</span></p>
           </Link>
           <ul className="list-none hidden sm:flex flex-row gap-10">
               {navLinks.map((Link) => (
